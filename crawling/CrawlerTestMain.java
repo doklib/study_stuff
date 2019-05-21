1e2cf706f55f7a4f5ca23003dabf8fb91c1ec000
doklib <49462599+doklib@users.noreply.github.com>
2019-05-21 16:39:20 +0900

    - 글 갯수 긁어오기, 작성일 가져오기, 리스트로 정리

--- crawling/CrawlerTestMain.java
+++ crawling/CrawlerTestMain.java
@@ -5,13 +5,12 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -29,21 +28,16 @@ public class CrawlerTestMain {
 
 		CrawlerTest c = new CrawlerTest();
 		
-//		String url = "https://search.naver.com/search.naver?date_from=&date_option=0&date_to=&dup_remove=1&nso=&post_blogurl=&post_blogurl_without=&query=%ED%85%8C%EC%8A%A4%ED%8A%B8%20%EA%B2%BD%EC%A3%BC&sm=tab_pge&srchby=all&st=sim&where=post&start=11";
-	//	String url = "https://search.naver.com/search.naver?date_from=&date_option=0&date_to=&dup_remove=1&nso=&post_blogurl=&post_blogurl_without=&query=%ED%85%8C%EC%8A%A4%ED%8A%B8&sm=tab_pge&srchby=all&st=sim&where=post&start=21";
-		//String url = "https://search.naver.com/search.naver?where=post&query=테스트+경주";
-			
 		String url = "https://search.naver.com/search.naver?date_from=&date_option=0&date_to=&dup_remove=1&nso=&post_blogurl=&post_blogurl_without=&query="+sc+"&sm=tab_pge&srchby=all&st=sim&where=post&start="+pg;
 		System.out.println(url);
+		System.out.println(pg+"번호");
 		
 		List<String> list = c.blog(url);
+		//int totalNum = Integer.parseInt(c.blogTotal(url));
 		
 		ArrayList<Object> myStringArrays = new ArrayList<Object>();
 		Map<String, Object> multiMap = new HashMap<>();
 
-		
-		
-		
 		int i = 0;
 		for(String s : list) {
 			String sb = c.getHtml(s);	
@@ -58,17 +52,29 @@ public class CrawlerTestMain {
 			String htmlList = nextDoc.select("div.se-component-content span").text();
 			
 			String dateTime = nextDoc.select("span.se_publishDate").text();
+			if(dateTime.contains("시간")) {
+				SimpleDateFormat format1 = new SimpleDateFormat ( "yyyy. MM. dd. HH:mm");		
+				Date time = new Date();
+				dateTime = format1.format(time);
+			}
+			else {
+				
+			}
+			System.out.println(dateTime);	
+			
 			// 블로그 내용 가져오기 테스트
-			System.out.println(htmlList);
 			System.out.println(s);
+			
+			String htmlList1 = htmlList.replace(",", "");
+			System.out.println(htmlList1);
+			
 			
 			Map<String, Object> contextMap = new HashMap<>();
 			contextMap.put("url", s);
 			contextMap.put("date", dateTime);
-			contextMap.put("text", htmlList);
+			contextMap.put("text", htmlList1);
 			
 			myStringArrays.add(contextMap);
-//			multiMap.put(s, contextMap);
 			
 			i++;
 		}
@@ -93,28 +99,11 @@ public class CrawlerTestMain {
 		       for(Object dom : myStringArrays) {
 		    	   	    	   
 		    	   
-		    	   String a = "\""+((HashMap)dom).get("url")+"\",\""+((HashMap)dom).get("date")+"\",\""+appendDQ(((HashMap)dom).get("text"))+"\"";
+		    	   String a = ((HashMap)dom).get("url")+","+((HashMap)dom).get("date")+",\""+((HashMap)dom).get("text")+"\"";
 		    	   fw.write(a);
 		    	   fw.newLine();
 		    	   resultCount++;
 		       }
-		       
-		       
-		       /*Set<Entry<String, Object>> set = ((Map<String, Object>) myStringArrays).entrySet();
-		       Iterator<Entry<String, Object>> itr = set.iterator();
-		       
-		       while (itr.hasNext())
-
-		       {
-		    	Map.Entry<String, Object> e = (Map.Entry<String, Object>)itr.next();
-		        System.out.println("no : " + e.getKey() + ", 내용 : " + e.getValue());
-		        fw.write(e.getKey()+",\""+e.getValue()+"\"");
-		        fw.newLine();
-		        resultCount++;
-		        if(resultCount % 100 == 0) {
-		            //log.info("resultCount :"+resultCount + "/" + list.size());
-		         }
-		        }*/
 		       
 		       fw.flush();
 		       fw.close();
@@ -126,10 +115,7 @@ public class CrawlerTestMain {
 		   }
 	
 
-	private static String appendDQ(Object object) {
-		// TODO Auto-generated method stub
-		return "\"" + object + "\"";
-	}
+
 
 	public static void main(String[] args) throws Exception{
 		
@@ -189,9 +175,16 @@ public class CrawlerTestMain {
 				//"테스트+경주";
 		//"%EA%B2%BD%EA%B8%B0%EB%8F%84%2B20%EB%8C%80%20%20%20%20%EB%82%A8%EC%9E%90%2B%2B%ED%9C%B4%EC%96%91%2B%EA%B0%80%EC%A1%B1%2B%EB%B4%84%2B%EA%B5%AD%EB%82%B4%EC%97%AC%ED%96%89%2B";
 		sc1 = value;
-		String res = null;
-		for(int i = 0; i <2; i++) {
+		
+		
+		CrawlerTest c = new CrawlerTest();
+		String url = "https://search.naver.com/search.naver?date_from=&date_option=0&date_to=&dup_remove=1&nso=&post_blogurl=&post_blogurl_without=&query="+sc+"&sm=tab_pge&srchby=all&st=sim&where=post&start=1";
+		int totalNum = Integer.parseInt(c.blogTotal(url));
+		
+		for(int i = 0; i <totalNum/100; i++) {
+//		for(int i = 0; i <5; i++) {	
 			getSearch(sc,i*10+1);
+			
 		}
 			
 	}
