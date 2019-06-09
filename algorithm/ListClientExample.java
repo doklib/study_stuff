3cd0b4b08fe09d9f454d847d939b6d81cef4f6bf
doklib <49462599+doklib@users.noreply.github.com>
2019-06-09 15:02:40 +0900

    - 교재의 실습을 따라해보았다.

---
+++ algorithm/ListClientExample.java
@@ -0,0 +1,27 @@
+package com.allendowney.thinkdast;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListClientExample {
+	@SuppressWarnings("rawtypes")
+	private List list;
+
+	@SuppressWarnings("rawtypes")
+	public ListClientExample() {
+		list = new ArrayList();
+	}
+
+	@SuppressWarnings("rawtypes")
+	public List getList() {
+		return list;
+	}
+
+	public static void main(String[] args) {
+		ListClientExample lce = new ListClientExample();
+		@SuppressWarnings("rawtypes")
+		List list = lce.getList();
+		System.out.println(list);
+	}
+}
